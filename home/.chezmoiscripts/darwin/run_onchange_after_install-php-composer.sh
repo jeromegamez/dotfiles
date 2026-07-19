f55ba1e416d5919d501a9055e2739796e12d2e17
Jérôme Gamez <jerome@gamez.name>
2026-07-20 00:55:07 +0200

    Install composer based on PHP being available

--- home/.chezmoiscripts/darwin/run_onchange_after_install-php-composer.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_install-php-composer.sh
@@ -2,15 +2,21 @@
 
 set -eufo pipefail
 
+PHP_BIN="$(command -v php || true)"
+if [[ -z "$PHP_BIN" ]]; then
+    echo "Skipping Composer setup because PHP is not installed"
+    exit 0
+fi
+
 COMPOSER_PATH="${HOME}/.local/bin"
 COMPOSER_FILE_NAME="composer"
 
 if [ ! -f "${COMPOSER_PATH}/${COMPOSER_FILE_NAME}" ]
 then
     mkdir -p "${COMPOSER_PATH}"
-    EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+    EXPECTED_CHECKSUM="$("$PHP_BIN" -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+    "$PHP_BIN" -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_CHECKSUM="$("$PHP_BIN" -r "echo hash_file('sha384', 'composer-setup.php');")"
 
     if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
     then
@@ -19,8 +25,8 @@ then
         exit 1
     fi
 
-    php composer-setup.php --install-dir="${COMPOSER_PATH}" --filename="${COMPOSER_FILE_NAME}"
+    "$PHP_BIN" composer-setup.php --install-dir="${COMPOSER_PATH}" --filename="${COMPOSER_FILE_NAME}"
     rm composer-setup.php
 fi
 
-${COMPOSER_PATH}/${COMPOSER_FILE_NAME} global update --quiet --with-all-dependencies
+"$PHP_BIN" "${COMPOSER_PATH}/${COMPOSER_FILE_NAME}" global update --quiet --with-all-dependencies
