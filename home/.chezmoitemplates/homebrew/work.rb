e5de9df39fe3593e1d8ac2af4cf30e440495cd33
Jérôme Gamez <jerome@gamez.name>
2026-08-12 10:15:33 +0200

    Add more required work apps

--- home/.chezmoitemplates/homebrew/work.rb
+++ home/.chezmoitemplates/homebrew/work.rb
@@ -1,17 +1,21 @@
 ## Work
 
+brew "d2" # Modern diagram scripting language that turns text to diagrams
 brew "eksctl" # Simple command-line tool for creating clusters on Amazon EKS
 brew "helm" # Kubernetes package manager
 brew "keidarcy/tap/e1s", trusted: true # manage AWS ECS resources, with a focus on Fargate
 brew "k9s" # Kubernetes CLI To Manage Your Clusters In Style!
 brew "mongodb/brew/mongodb-database-tools", trusted: true # Standard utilities for interacting with MongoDB.
 brew "terraform-docs" # Tool to generate documentation from Terraform modules
+brew "terrastruct/tap/tala", trusted: true # Advanced diagram layout engine for D2
 brew "tfautomv" # Generate Terraform moved blocks automatically for painless refactoring
 
 cask "copilot-cli" # Brings the power of Copilot coding agent directly to your terminal
+cask "corretto@25" # OpenJDK distribution from Amazon
 cask "github-copilot-app" # Native client for GitHub Copilot
 cask "intellij-idea" # Java IDE by JetBrains
 cask "mongodb-compass" # Interactive tool for analyzing MongoDB data
 cask "terraform-linters/tap/tflint", trusted: true # Pluggable Terraform linter
 cask "slack" # Team communication and collaboration software
+cask "tuple" # Remote pair programming app
 cask "yaak" # REST, GraphQL and gRPC client
